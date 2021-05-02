1f381b56868058ca1efbba56e4dd66a68ed5b3e9
Andrea Gori <Andrea.fr.gori@gmail.com>
2021-05-02 14:30:18 +0200

    header e footer completi

--- index.php
+++ index.php
@@ -54,12 +54,40 @@
     <title>Document</title>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
     <link rel="stylesheet" href="./assets/css/master.css">
   </head>
   <body>
+
     <header>
-    
+      <div class="nav-top">
+        <div class="nav-left">
+          <a href="">
+            <span>
+              <svg xmlns="http://www.w3.org/2000/svg" width="74" height="24" viewBox="0 0 74 24"><path fill="#4285F4" d="M9.24 8.19v2.46h5.88c-.18 1.38-.64 2.39-1.34 3.1-.86.86-2.2 1.8-4.54 1.8-3.62 0-6.45-2.92-6.45-6.54s2.83-6.54 6.45-6.54c1.95 0 3.38.77 4.43 1.76L15.4 2.5C13.94 1.08 11.98 0 9.24 0 4.28 0 .11 4.04.11 9s4.17 9 9.13 9c2.68 0 4.7-.88 6.28-2.52 1.62-1.62 2.13-3.91 2.13-5.75 0-.57-.04-1.1-.13-1.54H9.24z"/><path fill="#EA4335" d="M25 6.19c-3.21 0-5.83 2.44-5.83 5.81 0 3.34 2.62 5.81 5.83 5.81s5.83-2.46 5.83-5.81c0-3.37-2.62-5.81-5.83-5.81zm0 9.33c-1.76 0-3.28-1.45-3.28-3.52 0-2.09 1.52-3.52 3.28-3.52s3.28 1.43 3.28 3.52c0 2.07-1.52 3.52-3.28 3.52z"/><path fill="#4285F4" d="M53.58 7.49h-.09c-.57-.68-1.67-1.3-3.06-1.3C47.53 6.19 45 8.72 45 12c0 3.26 2.53 5.81 5.43 5.81 1.39 0 2.49-.62 3.06-1.32h.09v.81c0 2.22-1.19 3.41-3.1 3.41-1.56 0-2.53-1.12-2.93-2.07l-2.22.92c.64 1.54 2.33 3.43 5.15 3.43 2.99 0 5.52-1.76 5.52-6.05V6.49h-2.42v1zm-2.93 8.03c-1.76 0-3.1-1.5-3.1-3.52 0-2.05 1.34-3.52 3.1-3.52 1.74 0 3.1 1.5 3.1 3.54.01 2.03-1.36 3.5-3.1 3.5z"/><path fill="#FBBC05" d="M38 6.19c-3.21 0-5.83 2.44-5.83 5.81 0 3.34 2.62 5.81 5.83 5.81s5.83-2.46 5.83-5.81c0-3.37-2.62-5.81-5.83-5.81zm0 9.33c-1.76 0-3.28-1.45-3.28-3.52 0-2.09 1.52-3.52 3.28-3.52s3.28 1.43 3.28 3.52c0 2.07-1.52 3.52-3.28 3.52z"/><path fill="#34A853" d="M58 .24h2.51v17.57H58z"/><path fill="#EA4335" d="M68.26 15.52c-1.3 0-2.22-.59-2.82-1.76l7.77-3.21-.26-.66c-.48-1.3-1.96-3.7-4.97-3.7-2.99 0-5.48 2.35-5.48 5.81 0 3.26 2.46 5.81 5.76 5.81 2.66 0 4.2-1.63 4.84-2.57l-1.98-1.32c-.66.96-1.56 1.6-2.86 1.6zm-.18-7.15c1.03 0 1.91.53 2.2 1.28l-5.25 2.17c0-2.44 1.73-3.45 3.05-3.45z"/></svg>
+            </span>
+            <span class="privacy-title">Privacy e termini</span>
+          </a>
+        </div>
+        <div class="nav-right">
+          <a href=""><i class="fas fa-th"></i></a>
+          <a href=""><i class="fas fa-user-circle"></i></a>
+        </div>
+
+      </div>
+      
+      <div class="nav-bot">
+        <ul>
+          <li><a href="">Introduzione</a></li>
+          <li><a href="">Norme sulla privavy</a></li>
+          <li><a href="">Termini di servizio</a></li>
+          <li><a href="">Tecnologia</a></li>
+          <li><a class="active" href="">Domande frequenti</a></li>
+        </ul>
+      </div>
+      <div class="divider"></div>
     </header>
+
 
     <main>
       <div class="container">
@@ -73,7 +101,24 @@
     </main>
 
     <footer>
-    
+      <div class="container">
+        <div class="footer-left">
+          <ul>
+            <li><a href="">Google</a></li>
+            <li><a href="">Tutto su Google</a></li>
+            <li><a href="">Privacy</a></li>
+            <li><a href="">Termini</a></li>
+          </ul>
+        </div>
+        <div class="footer-right">
+          <i class="fas fa-globe"></i>
+          <select name="lenguages" id="lenguages">
+            <option value="it">Italiano</option>
+            <option value="eng">English</option>
+            <option value="deu">Deutsch</option>
+          </select>
+        </div>
+      </div>
     </footer>
 
   </body>
